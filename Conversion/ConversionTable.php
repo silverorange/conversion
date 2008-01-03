9be2567064e5303d7f547dd14c8c87f9e4a29bcf
Nathan Fredrickson <nathan@silverorange.com>
2008-01-03 16:23:54 +0000

    Placeholder that works with mysql too.
    svn commit r25304

--- Conversion/ConversionTable.php
+++ Conversion/ConversionTable.php
@@ -235,7 +235,7 @@ class ConversionTable
 
 		foreach ($this->fields as $field) {
 			if ($field->src_field === null)
-				$select_list[] = 'null';
+				$select_list[] = '0';
 			else
 				$select_list[] = $field->src_field->name;
 		}
